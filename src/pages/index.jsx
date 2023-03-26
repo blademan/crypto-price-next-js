90c268390645e2a8ad1a2c2d6d361e054278de1d
Eduard <blademan82@gmail.com>
2023-03-26 13:59:17 +0300

    up

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,17 +6,17 @@ import { useCoinData } from "../hooks/useCoinData";
 import { useInterval } from "../hooks/useInterval";
 
 export default function Home({ cryptocurrencies }) {
-  const { error, data, fetchData } = useCoinData(cryptocurrencies);
+  // const { error, data, fetchData } = useCoinData(cryptocurrencies);
 
-  useInterval(fetchData, 5000);
+  // useInterval(fetchData, 5000);
 
   if (!cryptocurrencies) <Spinner />;
 
-  if (error) <div>{error}</div>;
+  // if (error) <div>{error}</div>;
 
   return (
     <>
-      {data.map((coin) => (
+      {cryptocurrencies.map((coin) => (
         <CoinCard key={coin.id} coin={coin} />
       ))}
     </>
