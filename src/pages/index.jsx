3c95d8eb1bc0902a78e150d30b7a1b0c82846781
Eduard <blademan82@gmail.com>
2023-03-26 21:30:03 +0300

    add refactor

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,8 @@
-// import CoinCard from "@/components/CoinCard";
 import CoinCard from "../components/CoinCard";
 import { useCoinData } from "../utils/fetchCoinData";
 
 const Home = () => {
   const cryptocurrencies = useCoinData();
-
-  // const { data: cryptocurrencies } = useQuery(["coinData"], fetchCoinData, {
-  //   refetchInterval: 5000, // refetch data every 5 seconds
-  //   retry: 3, // retry up to 3 times if a request fails
-  // });
 
   return (
     <>
